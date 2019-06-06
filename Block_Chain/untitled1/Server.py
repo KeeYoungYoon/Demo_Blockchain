8939762618fb6731c01a3cb0ab738d3b5920d20f
KeeYoungYoon <44644430+KeeYoungYoon@users.noreply.github.com>
2019-06-06 15:14:09 +0900

    Update Server.py

--- Block_Chain/untitled1/Server.py
+++ Block_Chain/untitled1/Server.py
@@ -1,7 +1,13 @@
 import socketserver
 import threading
 
+import os
+
 import txManager
+import Block
+import KeyGenerator
+import ProofOfWork
+import time
 
 HOST = 'localhost' #Ip Blockchain server로 진행할 IP
 PORT = 9009
@@ -49,18 +55,68 @@ class MyTcpHandler(socketserver.BaseRequestHandler):
 
     def handle(self):
         print('[%s] is connected' % self.client_address[0])
-
+        save = []
         try:
             username = self.registerUsername()
             msg = self.request.recv(1024)
+
             while msg:
-                print(msg.decode())
-                if self.userman.messageHandler(username, msg.decode()) == -1:
-                    self.request.close()
-                    break
+                data = msg.decode('utf-8')
+                print(data)
+                save.append(data)
+                if len(save)%10 == 0:
+
+                    for name in self.userman.users:
+                        if "miner" in name:
+                            conn, addr = self.userman.users[name]
+                            for i in range(0, 10):
+                                tx = save[i]
+                                print(tx)
+                                conn.send(tx.encode())
+                                time.sleep(0.01)
+                            print('Block Received')#새로 추가된 부분 시작
+                            fr = open('block.txt', 'r')
+                            print('Successfully Opened Block')
+                            last_dict = fr.readlines()[-1].rstrip()
+                            hash, signature, public_key = Block.brick_hash(last_dict)
+                            print('Validation Process: ',KeyGenerator.validation(last_dict.encode('utf-8'), signature,KeyGenerator.readKey()))
+                            print('Send Block to Participants')
+                            self.userman.sendMessageToAll('Validated Block Sent')#여
+                            #validation
+                                #for j in range(100):
+                                #    continue
+                                #save.pop(0)
+                               # print(tx)
+                                #conn.send(tx.encode())
+                            break
+
                 msg = self.request.recv(1024)
+#                print(msg.decode())
+#                if self.userman.messageHandler(username, msg.decode()) == -1:
+#                    self.request.close()
+#                    break
+#                msg = self.request.recv(1024)
+#                data = msg.decode('utf-8')
+#                if data == "bc":
+#                    if os.path.isfile("key.pem") ==False:
+#                        KeyGenerator.create_key()
+#                    print("block!")
+#                    f = open('chat.txt', 'a+t')
+#                    f.writelines(str(save))
+#                    f.write('\n')
+#                    pow =  ProofOfWork.pow()
+#                    if pow == True:
+#                        Block.brick()
+#                        break
+#                else:
+#                    print(data)
+#                    save.append(data)
+#                data = msg.decode('utf-8')
+#                self.sendMessageToAll(data)
+#                msg = self.request.recv(1024)
 
         except Exception as e:
+            print('in error')
             print(e)
 
         print('[%s] Termination' % self.client_address[0])
